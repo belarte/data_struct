8e3ee97c24bf1bb647633de942bee1119bf17df7
Bruno Belarte <bruno.belarte@gmail.com>
2020-12-20 16:48:55 +0000

    Add Swapper interface and SimpeSwapper implementation

--- list/sort.go
+++ list/sort.go
@@ -25,6 +25,26 @@ func (comp *SimpleComparer) Count() int {
 	return comp.count
 }
 
+// Swaper swaps two ints
+type Swapper interface {
+	Swap(left, right *int)
+	Count() int
+}
+
+// SimpleSwaper inverts two ints and counts number of calls
+type SimpleSwapper struct {
+	count int
+}
+
+func (swap *SimpleSwapper) Swap(left, right *int) {
+	swap.count = swap.count + 1
+	*left, *right = *right, *left
+}
+
+func (swap *SimpleSwapper) Count() int {
+	return swap.count
+}
+
 // Sorter sorts list in place
 type Sorter interface {
 	Sort(List)
--- list/sort_test.go
+++ list/sort_test.go
@@ -53,3 +53,51 @@ func TestSimpleComparerCount(t *testing.T) {
 		}
 	}
 }
+
+func TestSimpleSwapperSwap(t *testing.T) {
+	tests := []struct {
+		left          int
+		right         int
+		expectedLeft  int
+		expectedRight int
+	}{
+		{0, 0, 0, 0},
+		{0, 1, 1, 0},
+		{42, 86, 86, 42},
+	}
+
+	var swapper list.Swapper = &list.SimpleSwapper{}
+	for _, test := range tests {
+		swapper.Swap(&test.left, &test.right)
+		if test.left != test.expectedLeft || test.right != test.expectedRight {
+			t.Errorf("Swap(%v, %v) != (%v, %v)", test.left, test.right, test.expectedLeft, test.expectedRight)
+		}
+	}
+}
+
+func TestSimpleSwapperCount(t *testing.T) {
+	tests := []struct {
+		call  bool
+		count int
+	}{
+		{false, 0},
+		{true, 1},
+		{false, 1},
+		{true, 2},
+		{true, 3},
+		{true, 4},
+		{false, 4},
+	}
+
+	var swapper list.Swapper = &list.SimpleSwapper{}
+	for _, test := range tests {
+		if test.call {
+			left, right := 0, 0
+			swapper.Swap(&left, &right)
+		}
+		got := swapper.Count()
+		if got != test.count {
+			t.Errorf("Expected count should be %v, but is %v", test.count, got)
+		}
+	}
+}
