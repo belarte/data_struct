bf9bf1d2bb112b05d281a466fc56b8b6a601ad93
Bruno Belarte <bruno.belarte@gmail.com>
2021-01-02 16:35:29 +0000

    Use random seed in main

--- main/main.go
+++ main/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/belarte/data_struct/list"
 	"math/rand"
+	"time"
 )
 
 func run(name string, size int, printer list.Printer) {
@@ -22,6 +23,8 @@ func main() {
 	size := flag.Int("size", 8, "Number of elements per list.")
 	algo := flag.String("algorithm", "", "Sorting algorith to use. If none specified, will run all algorithms.")
 	flag.Parse()
+
+	rand.Seed(time.Now().Unix())
 
 	switch *algo {
 	case "insertion", "selection":
