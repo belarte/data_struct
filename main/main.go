17a0a38a4e73a132cb3f60f003230dcebb922286
Bruno Belarte <bruno.belarte@gmail.com>
2021-01-02 15:40:10 +0000

    Command line argument to select algorithm and size of list

--- main/main.go
+++ main/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/belarte/data_struct/list"
 	"math/rand"
 )
 
-func run(name string, count int) {
-	fmt.Printf("Running %v sorter with list of %v elements\n", name, count)
-	l := rand.Perm(count)
+func run(name string, size int) {
+	fmt.Printf("Running %v sorter with list of size %v\n", name, size)
+	l := rand.Perm(size)
 	swapper := &list.SimpleSwapper{}
 	comparer := &list.LessThan{}
 	sorter := list.NewSorter(name, comparer, swapper)
@@ -18,6 +19,17 @@ func run(name string, count int) {
 }
 
 func main() {
-	run("insertion", 2048)
-	run("selection", 2048)
+	size := flag.Int("size", 8, "Number of elements per list.")
+	algo := flag.String("algorithm", "", "Sorting algorith to use. If none specified, will run all algorithms.")
+	flag.Parse()
+
+	switch *algo {
+	case "insertion", "selection":
+		run(*algo, *size)
+	case "":
+		run("insertion", *size)
+		run("selection", *size)
+	default:
+		fmt.Println("Unknown sorting algorithm...")
+	}
 }
