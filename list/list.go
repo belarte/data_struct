cefed9457c3f98f955c4e04a0ec9797353d95ad6
Bruno Belarte <bruno.belarte@gmail.com>
2021-01-05 18:01:09 +0000

    Split list package into multiple files

---
+++ list/list.go
@@ -0,0 +1,71 @@
+package list
+
+import "fmt"
+
+// List to be sorted
+type List []int
+
+// Comparer compares two ints
+type Comparer interface {
+	Compare(left, right int) bool
+	Count() int
+}
+
+// LessThan checks for equality and counts number of calls
+type LessThan struct {
+	count int
+}
+
+// Compare returns true if the ints are equal
+func (comp *LessThan) Compare(left, right int) bool {
+	comp.count++
+	return left < right
+}
+
+// Count returns the number of time Compare has been called
+func (comp *LessThan) Count() int {
+	return comp.count
+}
+
+// Swapper swaps two ints
+type Swapper interface {
+	Swap(left, right *int)
+	Count() int
+}
+
+// SimpleSwapper inverts two ints and counts number of calls
+type SimpleSwapper struct {
+	count int
+}
+
+// Swap swaps two ints in place and counts number of calls
+func (swap *SimpleSwapper) Swap(left, right *int) {
+	swap.count++
+	*left, *right = *right, *left
+}
+
+// Count returns the number of time Swap has been called
+func (swap *SimpleSwapper) Count() int {
+	return swap.count
+}
+
+// Printer displays a list
+type Printer interface {
+	// Print displays the list
+	Print(list List)
+}
+
+// NoPrint is to be used when no output is expected
+type NoPrint struct{}
+
+// Print does not do anything
+func (p *NoPrint) Print(list List) {
+}
+
+// CommandLinePrinter is to be used when an output on the command line is desired
+type CommandLinePrinter struct{}
+
+// Print displays the list on the command line
+func (p *CommandLinePrinter) Print(list List) {
+	fmt.Println(list)
+}
---
+++ list/list_test.go
@@ -0,0 +1,100 @@
+package list_test
+
+import (
+	"testing"
+
+	"github.com/belarte/data_struct/list"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimpleComparerCompare(t *testing.T) {
+	tests := []struct {
+		left     int
+		right    int
+		expected bool
+	}{
+		{0, 0, false},
+		{0, 1, true},
+		{1, 0, false},
+		{1, 1, false},
+		{42, 42, false},
+		{42, 86, true},
+		{86, 42, false},
+		{86, 86, false},
+	}
+
+	var comparer list.Comparer = &list.LessThan{}
+	for _, test := range tests {
+		got := comparer.Compare(test.left, test.right)
+		assert.Equal(t, test.expected, got, "%v < %v is %v", test.left, test.right, got)
+	}
+}
+
+func TestSimpleComparerCount(t *testing.T) {
+	tests := []struct {
+		call  bool
+		count int
+	}{
+		{false, 0},
+		{true, 1},
+		{false, 1},
+		{true, 2},
+		{true, 3},
+		{true, 4},
+		{false, 4},
+	}
+
+	var comparer list.Comparer = &list.LessThan{}
+	for _, test := range tests {
+		if test.call {
+			comparer.Compare(0, 0)
+		}
+		got := comparer.Count()
+		assert.Equal(t, test.count, got, "Expected count should be %v, but is %v", test.count, got)
+	}
+}
+
+func TestSimpleSwapperSwap(t *testing.T) {
+	tests := []struct {
+		left          int
+		right         int
+		expectedLeft  int
+		expectedRight int
+	}{
+		{0, 0, 0, 0},
+		{0, 1, 1, 0},
+		{42, 86, 86, 42},
+	}
+
+	var swapper list.Swapper = &list.SimpleSwapper{}
+	for _, test := range tests {
+		swapper.Swap(&test.left, &test.right)
+		assert.Equal(t, test.expectedLeft, test.left, "Left is %v but should be %v", test.left, test.expectedLeft)
+		assert.Equal(t, test.expectedRight, test.right, "Right is %v but should be %v", test.right, test.expectedRight)
+	}
+}
+
+func TestSimpleSwapperCount(t *testing.T) {
+	tests := []struct {
+		call  bool
+		count int
+	}{
+		{false, 0},
+		{true, 1},
+		{false, 1},
+		{true, 2},
+		{true, 3},
+		{true, 4},
+		{false, 4},
+	}
+
+	var swapper list.Swapper = &list.SimpleSwapper{}
+	for _, test := range tests {
+		if test.call {
+			left, right := 0, 0
+			swapper.Swap(&left, &right)
+		}
+		got := swapper.Count()
+		assert.Equal(t, test.count, got, "Expected count should be %v, but is %v", test.count, got)
+	}
+}
